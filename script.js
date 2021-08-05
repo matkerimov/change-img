29e3b3e4464fb5eb84d70eb26822f81643193cc7
zhaiyl07 <matkerimov.zhaiyl.ib9.1.19@gmail.com>
2021-08-05 16:34:08 +0600

    done

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const mainImg = document.querySelector(".main-img")
-const img = document.querySelectorAll(".img")
+const images = document.querySelectorAll(".img ")
 
 
-img.forEach(elem => {
-    elem.addEventListener("click", (event) => {
-        mainImg.src = elem.
+
+// images.forEach(item =>{
+//     item.addEventListener("click", ()=>{
+//         mainImg.src = item
+//     })
+// })
+
+
+images.forEach(item => {
+    item.addEventListener("click", (event) => {
+        mainImg.src = item.src
             images.forEach((elem) => elem.classList.remove("border"))
         event.target.classList.add("border")
     })
 })
-
 
 
 
